e277e41989257b67ad5afeb75696d802a65b7897
Athul Krishna Radhakrishnan Nair <athulkrishnar1998@gmail.com>
2025-09-11 12:28:28 -0400

    Updated run function

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,17 +3,17 @@ import subprocess
 
 
 def run_python_file(working_directory, file_path, args=[]):
-    file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     abs_working_dir = os.path.abspath(working_directory)
-    if not file_path.startswith(os.path.abspath(working_directory)):
+    if not abs_file_path.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File "{file_path}" not found.'
-    if not file_path.endswith(".py"):
-        return f'Error: "{file_path}" is not a Python file.'
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{abs_file_path}" is not a Python file.'
 
     try:
-        final_args = ["python3", file_path]
+        final_args = ["python3", abs_file_path]
         final_args.extend(args)
         completed_process = subprocess.run(
             final_args, cwd=abs_working_dir, timeout=30, capture_output=True
@@ -21,7 +21,7 @@ def run_python_file(working_directory, file_path, args=[]):
         if completed_process.stdout or completed_process.stderr:
             final_string = f"STDOUT: {completed_process.stdout} STDERR:{completed_process.stderr} "
         else:
-            final_string = "No output produced." 
+            final_string = "No output produced.\n" 
         if completed_process.returncode != 0:
             final_string += f"Process exited with code {completed_process.returncode}"
         return final_string
